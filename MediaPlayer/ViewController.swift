8e720233839485b3579d87eadd3c32afd4a80b8e
nkone <phtruong@protonmail.com>
2019-10-04 19:17:57 -0700

    testing

--- MediaPlayer/ViewController.swift
+++ MediaPlayer/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello ")
+        print("hi there")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
