fc18d6c2bd9cb475e74f3a8795431c5f2ce1a628
nkone <phtruong@protonmail.com>
2019-10-05 20:50:29 -0700

    revert back to older version

---
+++ MediaPlayer/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MediaPlayer
+//
+//  Created by Phat Truong on 10/4/19.
+//  Copyright © 2019 Phat Truong. All rights reserved.
+//
+
+import UIKit
+
+var linkTest = ""
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var linkField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func submitButton(_ sender: Any) {
+        if linkField.text != "" {
+            linkTest = linkField.text!
+           // performSegue(withIdentifier: "submitSegue", sender: self)
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
